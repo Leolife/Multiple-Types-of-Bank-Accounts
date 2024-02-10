a8a9d7549bca6ac65ab4b72300c0f1d7cf681751
Leo Roth <leoroth123@yahoo.com>
2024-02-09 19:20:05 -0800

    fixed certificateOfDeposit class

--- cpp_files/certificateOfDeposit.cpp
+++ cpp_files/certificateOfDeposit.cpp
@@ -1,1 +1,136 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "bankAccount.h"
 #include "certificateOfDeposit.h"
+
+using namespace std;
+
+certificateOfDeposit::certificateOfDeposit()
+{
+    interestRate = 0.0;//YEARLY interest rate
+    maturityMonths = 0;
+    cdMonths = 0;
+}
+certificateOfDeposit::~certificateOfDeposit() {};
+
+double certificateOfDeposit::getInterestRate()
+{
+    return interestRate;
+}
+int certificateOfDeposit::getMaturityMonths()
+{
+    return maturityMonths;
+}
+int certificateOfDeposit::getCurrentCDMonth()
+{
+    return cdMonths;
+}
+
+void certificateOfDeposit::setInterestRate(double intRate)
+{
+    interestRate = intRate;
+}
+void certificateOfDeposit::setMaturityMonths(int month)
+{
+    maturityMonths = month;
+}
+void certificateOfDeposit::setCurrentCDMonth(int cmonth)
+{
+    cdMonths = cmonth;
+}
+
+void certificateOfDeposit::postInterest()
+{
+    setInterestRate();
+    cout << "Annual interest rate: " << getInterestRate() << "%" << endl;
+}
+
+//withdraw function with maturity month verification built in
+void certificateOfDeposit::withdraw()
+{
+    string answer;
+    double amount;
+    bool firstLoop = false;
+    bool secondLoop = false;
+    setInterestRate();
+    setMaturityMonths();
+    setBalance(10000);//assumed initial balance for sake of this particular CD transaction scenaio
+
+    cout << "Initial balance: $" << balance << endl;
+
+    cout << "Amount of months it has been for your CD: ";
+    cin >> cdMonths;
+    balance = balance + (balance * ((cdMonths / static_cast<double>(12)) * (getInterestRate() / 100)));
+    cin.clear();//lines 73 and 74 make sure the program does not endlessly loop with the same input
+    cin.ignore(1000, '\n');
+    if (cdMonths < 6)
+    {
+        cout << "Cannot withdraw until maturity month is reached. Maturity month: " << getMaturityMonths() << ".\n";
+    }
+    else if (cdMonths >= 6)
+    {
+        while (true)
+        {
+            cout << "\nCurrent balance (including interest): $" << fixed << setprecision(2) << balance << endl;
+            cout << "Would you like to make a withdrawal? (Y or N): ";
+            cin >> answer;
+
+            if (answer == "Y" || answer == "y")
+            {
+                cout << "Withdrawal amount: $";
+                cin >> amount;
+                cin.clear();//lines 91 and 92 make sure the program does not endlessly loop with the same input
+                cin.ignore(1000, '\n');
+                balance -= amount;
+                if (balance < 0)
+                {
+                    balance += amount;
+                    cout << "\nError: amount exceeds balance" << endl;
+                }
+            }
+            else if (answer == "N" || answer == "n")
+            {
+                cout << endl;
+                break;
+            }
+            else
+            {
+                cout << "Invalid response" << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "Invalid response" << endl;
+    }
+}
+
+void certificateOfDeposit::createMonthlyStatement()
+{
+    setName("Leo");
+    setAccountNumber();
+    setInterestRate();
+    //printing account details
+    cout << setw(30) << "-------------------\n" << "|--------";
+    cout << "| Monthly Statement |--------|" << endl;
+    cout << setw(30) << "------------------- " << endl;
+    cout << setw(27) << "Account holder: " << getName() << endl;
+    cout << setw(27) << "Account number: " << getAccountNumber() << endl;
+    cout << setw(25) << "Current balance" << endl;
+    cout << setw(28) << "(including interest): $" << balance << endl;
+    cout << setw(5) << "";
+    postInterest();
+    cout << "|-------------------------------------|\n";
+}
+
+void certificateOfDeposit::print()
+{
+    cout << setw(35) << "------------------------\n" << "|--------";
+    cout << "| Certificate of Deposit |--------|" << endl;
+    cout << setw(35) << "------------------------ " << endl << endl;
+    withdraw();
+    createMonthlyStatement();
+    cout << endl << endl;
+}
--- header_files/certificateOfDeposit.h
+++ header_files/certificateOfDeposit.h
@@ -14,132 +14,29 @@ private:
     int cdMonths;//amount of months since initial deposit
 
 public:
-    certificateOfDeposit()
-    {
-        interestRate = 0.0;//YEARLY interest rate
-        maturityMonths = 0;
-        cdMonths = 0;
-    }
+    certificateOfDeposit();
+
     ~certificateOfDeposit() {};
 
-    double getInterestRate()
-    {
-        return interestRate;
-    }
-    int getMaturityMonths()
-    {
-        return maturityMonths;
-    }
-    int getCurrentCDMonth()
-    {
-        return cdMonths;
-    }
+    double getInterestRate();
 
-    void setInterestRate(double intRate = 5)
-    {
-        interestRate = intRate;
-    }
-    void setMaturityMonths(int month = 6)
-    {
-        maturityMonths = month;
-    }
-    void setCurrentCDMonth(int cmonth)
-    {
-        cdMonths = cmonth;
-    }
+    int getMaturityMonths();
 
-    void postInterest()
-    {
-        setInterestRate();
-        cout << "Annual interest rate: " << getInterestRate() << "%" << endl;
-    }
+    int getCurrentCDMonth();
+
+    void setInterestRate(double intRate = 5);
+
+    void setMaturityMonths(int month = 6);
+
+    void setCurrentCDMonth(int cmonth);
+
+    void postInterest();
 
     //withdraw function with maturity month verification built in
-    void withdraw() override
-    {
-        string answer;
-        double amount;
-        bool firstLoop = false;
-        bool secondLoop = false;
-        setInterestRate();
-        setMaturityMonths();
-        setBalance(10000);//assumed initial balance for sake of this particular CD transaction scenaio
+    void withdraw() override;
 
-        cout << "Initial balance: $" << balance << endl;
+    void createMonthlyStatement() override;
 
-        cout << "Amount of months it has been for your CD: ";
-        cin >> cdMonths;
-        balance = balance + (balance * ((cdMonths / static_cast<double>(12)) * (getInterestRate() / 100)));
-        cin.clear();//lines 73 and 74 make sure the program does not endlessly loop with the same input
-        cin.ignore(1000, '\n');
-        if (cdMonths < 6)
-        {
-            cout << "Cannot withdraw until maturity month is reached. Maturity month: " << getMaturityMonths() << ".\n";
-        }
-        else if (cdMonths >= 6)
-        {
-            while (true)
-            {
-                cout << "\nCurrent balance (including interest): $" << fixed << setprecision(2) << balance << endl;
-                cout << "Would you like to make a withdrawal? (Y or N): ";
-                cin >> answer;
-
-                if (answer == "Y" || answer == "y")
-                {
-                    cout << "Withdrawal amount: $";
-                    cin >> amount;
-                    cin.clear();//lines 91 and 92 make sure the program does not endlessly loop with the same input
-                    cin.ignore(1000, '\n');
-                    balance -= amount;
-                    if (balance < 0)
-                    {
-                        balance += amount;
-                        cout << "\nError: amount exceeds balance" << endl;
-                    }
-                }
-                else if (answer == "N" || answer == "n")
-                {
-                    cout << endl;
-                    break;
-                }
-                else
-                {
-                    cout << "Invalid response" << endl;
-                }
-            }
-        }
-        else
-        {
-            cout << "Invalid response" << endl;
-        }
-    }
-
-    void createMonthlyStatement() override
-    {
-        setName("Leo");
-        setAccountNumber();
-        setInterestRate();
-        //printing account details
-        cout << setw(30) << "-------------------\n" << "|--------";
-        cout << "| Monthly Statement |--------|" << endl;
-        cout << setw(30) << "------------------- " << endl;
-        cout << setw(27) << "Account holder: " << getName() << endl;
-        cout << setw(27) << "Account number: " << getAccountNumber() << endl;
-        cout << setw(25) << "Current balance" << endl;
-        cout << setw(28) << "(including interest): $" << balance << endl;
-        cout << setw(5) << "";
-        postInterest();
-        cout << "|-------------------------------------|\n";
-    }
-
-    void print()
-    {
-        cout << setw(35) << "------------------------\n" << "|--------";
-        cout << "| Certificate of Deposit |--------|" << endl;
-        cout << setw(35) << "------------------------ " << endl << endl;
-        withdraw();
-        createMonthlyStatement();
-        cout << endl << endl;
-    }
+    void print();
 };
 
