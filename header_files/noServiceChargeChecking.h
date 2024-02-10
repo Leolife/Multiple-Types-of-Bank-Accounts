7f1a0270f660685d74ec31549168f11fc853b071
Leo Roth <leoroth123@yahoo.com>
2024-02-09 19:34:37 -0800

    fixed noServiceChargeChecking class

--- header_files/noServiceChargeChecking.h
+++ header_files/noServiceChargeChecking.h
@@ -13,149 +13,25 @@ protected:
     double interestRate;
 
 public:
-    noServiceChargeChecking()
-    {
-        minimumBalance = 0.0;
-        interestRate = 0.0;
-    }
+    noServiceChargeChecking();
+
     ~noServiceChargeChecking() {};
 
-    double getMinimumBalance()
-    {
-        return minimumBalance;
-    }
-    double getInterestRate()
-    {
-        return interestRate;
-    }
+    double getMinimumBalance();
 
-    void setMinimumBalance(double minBal = 50.0)
-    {
-        minimumBalance = minBal;
-    }
-    void setInterestRate(double intRate = 0.01)
-    {
-        interestRate = intRate;
-    }
+    double getInterestRate();
+
+    void setMinimumBalance(double minBal = 50.0);
+    
+    void setInterestRate(double intRate = 0.01);
 
     //write check function with the minimum balance verification built in
-    void writeCheck() override
-    {
-        setMinimumBalance();
-        string answer, answer1;
-        double amount;
-        {
-            while (true)
-            {
-                //writing a check to withdraw or deposit money
-                cout << "\n -----\n|Check|\n -----\nCurrent Balance: $" << balance << "\nWould you like to make a withdrawal / deposit? (Y or N): ";
-                cin >> answer;
-
-                if (answer == "Y" || answer == "y")
-                {
-                    cout << "Withdrawal or Deposit? (W or D): ";
-                    cin >> answer1;
-                    if (answer1 == "W" || answer1 == "w")
-                    {
-                        cout << "Withdrawal amount: $";
-                        cin >> amount;
-                        cin.clear();//lines 62 and 63 make sure the program does not endlessly loop with the same input
-                        cin.ignore(1000, '\n');
-                        balance -= amount;
-                        if (balance < getMinimumBalance())
-                        {
-                            balance += amount;
-                            cout << "Error: balance cannot fall under minimum required balance.\n";
-                        }
-                    }
-                    else if (answer1 == "D" || answer1 == "d")
-                    {
-                        cout << "Deposit amount: $";
-                        cin >> amount;
-                        cin.clear();//lines 75 and 76 make sure the program does not endlessly loop with the same input
-                        cin.ignore(1000, '\n');
-                        balance += amount;
-                    }
-                    else
-                    {
-                        cout << "Invalid response" << endl;
-                    }
-                }
-                else if (answer == "N" || answer == "n")
-                {
-                    cout << endl;
-                    break;
-                }
-                else
-                {
-                    cout << "Invalid response" << endl;
-                }
-            }
-        }
-    }
+    void writeCheck() override;
 
     //withdraw function with the minimum balance verification built in
-    void withdraw() override
-    {
-        string answer;
-        double amount;
-        setMinimumBalance();
+    void withdraw() override;
 
-        while (true)
-        {
-            cout << "Balance: $" << balance << endl;
-            cout << "Would you like to make a withdrawal? (Y or N): ";
-            cin >> answer;
+    virtual void createMonthlyStatement();
 
-            if (answer == "Y" || answer == "y")
-            {
-                cout << "Withdrawal amount: $";
-                cin >> amount;
-                balance -= amount;
-                if (balance < getMinimumBalance())
-                {
-                    balance += amount;
-                    cout << "Error: balance cannot fall under minimum required balance.\n";
-                }
-            }
-            else if (answer == "N" || answer == "n")
-            {
-                cout << endl;
-                break;
-            }
-            else
-            {
-                cout << "Invalid response" << endl;
-            }
-        }
-    }
-
-    virtual void createMonthlyStatement()
-    {
-        setName("Leo");
-        setAccountNumber();
-        setInterestRate();
-        //printing account details
-        cout << setw(30) << "-------------------\n" << "|--------";
-        cout << "| Monthly Statement |--------|" << endl;
-        cout << setw(30) << "------------------- " << endl;
-        cout << setw(27) << "Account holder: " << getName() << endl;
-        cout << setw(27) << "Account number: " << getAccountNumber() << endl;
-        cout << setw(28) << "Balance: $" << getBalance() << endl;
-        cout << setw(28) << "Minimum required balance: $" << getMinimumBalance() << endl;
-        cout << setw(27) << "Interest rate: " << getInterestRate() << "%" << endl;
-        cout << "|-------------------------------------|\n";
-    }
-
-    virtual void print()
-    {
-        setMinimumBalance();
-        cout << setw(47) << "------------------------------------\n" << "|--------";
-        cout << "| No Service Charge Checking Account |--------|" << endl;
-        cout << setw(47) << "------------------------------------ " << endl;
-        cout << setw(37) << "MINIMUM BALANCE: $" << getMinimumBalance() << endl;
-        writeCheck();
-        createMonthlyStatement();
-    }
+    virtual void print();
 };
-
