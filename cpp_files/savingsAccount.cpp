cbabb160d3f4bd5ad140d15bade94f9a68dcd791
Leo Roth <leoroth123@yahoo.com>
2024-02-09 19:36:20 -0800

    fixed savingsAccount class

--- cpp_files/savingsAccount.cpp
+++ cpp_files/savingsAccount.cpp
@@ -1,1 +1,57 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "bankAccount.h"
 #include "savingsAccount.h"
+
+using namespace std;
+
+    savingsAccount::savingsAccount()
+    {
+        interestRate = 0.0;
+    }
+    savingsAccount::~savingsAccount() {};
+
+    double savingsAccount::getInterestRate()
+    {
+        return interestRate;
+    }
+
+    void savingsAccount::setInterestRate(double intRate)
+    {
+        interestRate = intRate;
+    }
+
+    void savingsAccount::postInterest()
+    {
+        setInterestRate();
+        cout << "Interest rate: " << getInterestRate() << "%" << endl;
+    }
+
+    void savingsAccount::createMonthlyStatement()
+    {
+        setName("Leo");
+        setAccountNumber();
+        setInterestRate();
+        //printing account details
+        cout << setw(30) << "-------------------\n" << "|--------";
+        cout << "| Monthly Statement |--------|" << endl;
+        cout << setw(30) << "------------------- " << endl;
+        cout << setw(27) << "Account holder: " << getName() << endl;
+        cout << setw(27) << "Account number: " << getAccountNumber() << endl;
+        cout << setw(28) << "Balance: $" << getBalance() << endl;
+        cout << setw(12) << "";
+        postInterest();
+        cout << "|-------------------------------------|\n";
+    }
+
+    void savingsAccount::print()
+    {
+        cout << setw(28) << "-----------------\n" << "|--------";
+        cout << "| Savings Account |--------|" << endl;
+        cout << setw(28) << "----------------- " << endl << endl;
+        deposit();
+        withdraw();
+        createMonthlyStatement();
+    }
--- header_files/savingsAccount.h
+++ header_files/savingsAccount.h
@@ -12,53 +12,17 @@ protected:
     double interestRate;
 
 public:
-    savingsAccount()
-    {
-        interestRate = 0.0;
-    }
+    savingsAccount();
+
     ~savingsAccount() {};
 
-    double getInterestRate()
-    {
-        return interestRate;
-    }
+    double getInterestRate();
 
-    void setInterestRate(double intRate = 0.03)
-    {
-        interestRate = intRate;
-    }
+    void setInterestRate(double intRate = 0.03);
 
-    void postInterest()
-    {
-        setInterestRate();
-        cout << "Interest rate: " << getInterestRate() << "%" << endl;
-    }
+    void postInterest();
 
-    virtual void createMonthlyStatement()
-    {
-        setName("Leo");
-        setAccountNumber();
-        setInterestRate();
-        //printing account details
-        cout << setw(30) << "-------------------\n" << "|--------";
-        cout << "| Monthly Statement |--------|" << endl;
-        cout << setw(30) << "------------------- " << endl;
-        cout << setw(27) << "Account holder: " << getName() << endl;
-        cout << setw(27) << "Account number: " << getAccountNumber() << endl;
-        cout << setw(28) << "Balance: $" << getBalance() << endl;
-        cout << setw(12) << "";
-        postInterest();
-        cout << "|-------------------------------------|\n";
-    }
+    virtual void createMonthlyStatement();
 
-    virtual void print()
-    {
-        cout << setw(28) << "-----------------\n" << "|--------";
-        cout << "| Savings Account |--------|" << endl;
-        cout << setw(28) << "----------------- " << endl << endl;
-        deposit();
-        withdraw();
-        createMonthlyStatement();
-    }
+    virtual void print();
 };
-
